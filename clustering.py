c2301b648ed81641229ac07e1fe9d90883fc9062
Jimson Huang <jimsonjy@gmail.com>
2022-10-18 10:20:24 -0700

    Bug fixes

--- clustering.py
+++ clustering.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 def K_Means(X: np.ndarray, K: int, mu: np.ndarray) -> np.ndarray:
+    assert K > 0
     # If mu not given, randomly choose K data points
     if mu.size == 0:
         assert X.shape[0] >= K
@@ -13,7 +14,7 @@ def K_Means(X: np.ndarray, K: int, mu: np.ndarray) -> np.ndarray:
             distances = [np.linalg.norm(X[i] - mu[j]) for j in range(mu.shape[0])]
             clusters[distances.index(min(distances))].append(X[i])
 
-        new_mu = np.array([np.mean(i) for i in clusters])
-        if (new_mu == mu).all():
+        new_mu = np.array([np.mean(i, axis=0) for i in clusters])
+        if np.array_equal(new_mu, mu):
             return mu
         mu = new_mu
